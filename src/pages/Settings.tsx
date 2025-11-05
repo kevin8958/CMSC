112e1bf2aa4a0099838f2fb1137eec71ce740a5b
kevin8958 <kevin8958@gmail.com>
2025-11-05 18:08:57 +0900

    feat: 로그아웃 기능 임시 구현

--- src/pages/Settings.tsx
+++ src/pages/Settings.tsx
@@ -1,8 +1,41 @@
-function Settings() {
+import { supabase } from "@/lib/supabase";
+import { useNavigate } from "react-router-dom";
+import Button from "@/components/Button";
+import FlexWrapper from "@/layout/FlexWrapper";
+import Typography from "@/foundation/Typography";
+
+function SettingsPage() {
+  const navigate = useNavigate();
+
+  const onLogout = async () => {
+    await supabase.auth.signOut(); // ✅ 세션 제거
+    navigate("/login"); // ✅ 로그인페이지로 이동
+  };
+
   return (
-    <div>
-      <h1>설정</h1>
-    </div>
+    <FlexWrapper classes="w-full" items="center" justify="center" gap={4}>
+      <FlexWrapper classes="w-[400px]" direction="col" gap={4}>
+        <Typography variant="H3">설정</Typography>
+        <FlexWrapper
+          classes="h-[400px] border rounded-md p-3"
+          direction="col"
+          items="center"
+          justify="between"
+        >
+          <p>추가예정</p>
+          <Button
+            variant="contain"
+            color="primary"
+            size="md"
+            classes="w-full"
+            onClick={onLogout}
+          >
+            로그아웃
+          </Button>
+        </FlexWrapper>
+      </FlexWrapper>
+    </FlexWrapper>
   );
 }
-export default Settings;
+
+export default SettingsPage;
