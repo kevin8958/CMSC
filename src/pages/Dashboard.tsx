a453427158f92e322ffc7c6d7bac8689d0d2613e
kevin8958 <kevin8958@gmail.com>
2026-02-09 13:33:54 -0800

    fix: 대시보드 레이아웃 수정

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -17,7 +17,8 @@ function Dashboard() {
   }, [currentCompanyId]);
 
   return (
-    <div className="grid grid-cols-12 grid-rows-[0.6fr_0.4fr] gap-2 pb-2 h-full">
+    <div className="grid grid-cols-12 gap-2 pb-2 h-full">
+      {/* grid-rows-[0.6fr_0.4fr] */}
       {/* 상단 Row: 높이 60% 차지 */}
       <div className="col-span-12 xl:col-span-6 bg-white border rounded-xl h-full overflow-hidden">
         <MonthSchedule />
@@ -25,14 +26,13 @@ function Dashboard() {
       <div className="col-span-12 xl:col-span-6 bg-white border rounded-xl h-full overflow-hidden">
         <CheckList />
       </div>
-
       {/* 하단 Row: 높이 40% 차지 */}
-      <div className="col-span-12 xl:col-span-4 border rounded-xl bg-white h-full overflow-hidden">
+      {/* <div className="col-span-12 xl:col-span-4 border rounded-xl bg-white h-full overflow-hidden">
         <DepartmentDoughnutChart workers={allList} />
       </div>
       <div className="col-span-12 xl:col-span-8 bg-white border rounded-xl h-full overflow-hidden">
         <YearlyIncomeChart />
-      </div>
+      </div> */}
     </div>
   );
 }
